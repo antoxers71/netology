f99927ca4754f58bd6a0db1b0f063fb392a03e3f
antonpopkov <mr.antoxa@mail.ru>
2024-03-07 00:22:56 +0300

    [002-console] task1 fix by review, add yargs command

--- 002-console/task_1/get-date.js
+++ 002-console/task_1/get-date.js
@@ -3,78 +3,101 @@ const yargs = require('yargs/yargs')
 const {hideBin} = require('yargs/helpers')
 
 const argv = yargs(hideBin(process.argv))
-    .option('current', {
-        alias: 'c',
-        type: 'boolean',
-        description: 'текущий период'
+    .command('current [year] [month] [day]', 'Получение текущей даты', (yargs) => {
+        return yargs
+            .option('year', {
+                alias: 'y',
+                type: 'boolean',
+                description: 'Получение текущего года'
+            })
+            .option('month', {
+                alias: 'm',
+                type: 'boolean',
+                description: 'Получение текущего месяца'
+            })
+            .option('day', {
+                alias: 'd',
+                type: 'boolean',
+                description: 'Получение текущего дня'
+            })
+    }, (argv) => {
+        if (argv.year) {
+            console.log(new Date().getFullYear());
+            return
+        }
+        if (argv.month) {
+            console.log(new Date().getMonth() + 1);
+            return;
+        }
+        if (argv.day) {
+            console.log(new Date().getDate());
+            return;
+        }
+
+        console.log(new Date().toISOString());
     })
-    .option('add', {
-        alias: 'a',
-        type: 'boolean',
-        description: 'добавить период'
+    .command('sub [year] [month] [day]', 'Вычесть период из текущей даты', (yargs) => {
+        return yargs
+            .option('year', {
+                alias: 'y',
+                type: 'number',
+                description: 'Кол-во лет'
+            })
+            .option('month', {
+                alias: 'm',
+                type: 'number',
+                description: 'Кол-во месяцев'
+            })
+            .option('day', {
+                alias: 'd',
+                type: 'number',
+                description: 'Кол-во дней'
+            })
+    }, (argv) => {
+        const date = new Date();
+
+        if (argv.year) {
+            date.setFullYear(new Date().getFullYear() - argv.year);
+        }
+        if (argv.month) {
+            date.setMonth(new Date().getMonth() - argv.month);
+        }
+        if (argv.day) {
+            date.setDate(new Date().getDate() - argv.day);
+        }
+
+        console.log(date.toISOString());
     })
-    .option('sub', {
-        alias: 's',
-        type: 'boolean',
-        description: 'отнять период'
-    })
-    .option('year', {
-        alias: 'y',
-        type: 'number',
-        description: 'год'
-    })
-    .option('month', {
-        alias: 'm',
-        type: 'number',
-        description: 'месяц',
-    })
-    .option('day', {
-        alias: 'd',
-        type: 'number',
-        description: 'день',
+    .command('add [year] [month] [day]', 'Прибавить период к текущей дате', (yargs) => {
+        return yargs
+            .option('year', {
+                alias: 'y',
+                type: 'number',
+                description: 'Кол-во лет'
+            })
+            .option('month', {
+                alias: 'm',
+                type: 'number',
+                description: 'Кол-во месяцев'
+            })
+            .option('day', {
+                alias: 'd',
+                type: 'number',
+                description: 'Кол-во дней'
+            })
+    }, (argv) => {
+        const date = new Date();
+
+        if (argv.year) {
+            date.setFullYear(new Date().getFullYear() + argv.year);
+        }
+        if (argv.month) {
+            date.setMonth(new Date().getMonth() + argv.month);
+        }
+        if (argv.day) {
+            date.setDate(new Date().getDate() + argv.day);
+        }
+
+        console.log(date.toISOString());
     })
     .argv
-
-const date = new Date();
-
-if (argv.sub) {
-    if (argv.year) {
-        date.setFullYear(new Date().getFullYear() - argv.year);
-    }
-    if (argv.month) {
-        date.setMonth(new Date().getMonth() - argv.month);
-    }
-    if (argv.day) {
-        date.setDate(new Date().getDate() - argv.day);
-    }
-}
-
-if (argv.add) {
-    if (argv.year) {
-        date.setFullYear(new Date().getFullYear() + argv.year);
-    }
-    if (argv.month) {
-        date.setMonth(new Date().getMonth() + argv.month);
-    }
-    if (argv.day) {
-        date.setDate(new Date().getDate() + argv.day);
-    }
-}
-
-if (argv.current) {
-    if (argv.hasOwnProperty('year')) {
-        console.log(new Date().getFullYear());
-        return;
-    }
-    if (argv.hasOwnProperty('month')) {
-        console.log(new Date().getMonth() + 1);
-        return;
-    }
-    if (argv.hasOwnProperty('day')) {
-        console.log(new Date().getDate());
-        return;
-    }
-}
-
-console.log(date.toISOString())
-
